6a35dc26a20fa3d59a49c40753e1cb74ea110c35
Nick08020608 <nicolas.paulo.vega06@gmail.com>
2023-05-19 09:42:38 -0500

    l1

--- python/ejercicio.py
+++ python/ejercicio.py
@@ -1,1 +1,11 @@
 #Diseñe un algoritmo que capture dos números, y que realice la suma de dichos números, y mostrar los datos si es el resultado no es negativo.
+
+number= int(input("Ingrese un numero: "))
+number_two= int(input("Ingrese un numero. "))
+ 
+suma= number+number_two
+
+if suma >0:
+    print("El valor de la suma es: ", suma)
+else:
+    print("Devido a que es un numero negativo no puedo realizar la operacion...")
