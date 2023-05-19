0558b72e6869a7da6205b6c224c4d1832696c37f
Nick08020608 <nicolas.paulo.vega06@gmail.com>
2023-05-19 11:33:09 -0500

    l4

---
+++ python/ejercicio4.py
@@ -0,0 +1,10 @@
+#Design an algorithm that reads 2 numbers and displays whether they are positive.
+
+number_one= int(input("Enter a value: "))
+number_two= int(input("Enter a value: "))
+
+if number_one and number_two >0:
+    print("It is a positive value.")
+else:
+    print("is a negative value.")
+
---
+++ python/ejercicio6.py
@@ -0,0 +1,7 @@
+#Crear un algoritmo que lea el nombre de un empleado, su salario básico por hora, el nro. de horas trabajadas en un mes.
+
+minimum_salary= 1030000
+transportation_subsidy= 70000
+days_worked= int(input("Enter the days on which I work: "))
+daily_wage= minimum_salary/days_worked
+
