80301c89c637c6bf3ed0ca8f93c5fb8a524c7a3b
Nick08020608 <nicolas.paulo.vega06@gmail.com>
2023-05-19 09:46:37 -0500

    l2

---
+++ python/ejercicio2.py
@@ -0,0 +1,8 @@
+#Diseñe un algoritmo que, al ingresar la edad del usuario, si es mayor de edad mostrar el un mensaje “es mayor de edad”.
+
+edad = int(input("Ingrese su edad: "))
+
+if edad >=18:
+    print("Usted es mayor de edad")
+else:
+    print("Usted es menor de edad")
