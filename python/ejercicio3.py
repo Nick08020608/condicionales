c0cb76d29cc1d0444e3a309ef497e60c901c102f
Nick08020608 <nicolas.paulo.vega06@gmail.com>
2023-05-19 09:50:45 -0500

    l2

---
+++ python/ejercicio3.py
@@ -0,0 +1,1 @@
+#Diseñe un algoritmo que guarde el nombre del aprendiz, nombre del curso, nota definitiva, el numero de clases en el semestre y el número de las fallas. En el caso de que las fallas superen el 30% del número de clases se debe mostrar la nota que no aprobó y se calificara cero(0)
