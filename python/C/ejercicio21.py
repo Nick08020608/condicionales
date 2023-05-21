0bf51a987e19039b54483432340c164a5d6a3795
Nick08020608 <nicolas.paulo.vega06@gmail.com>
2023-05-21 16:57:26 -0500

    Update ejercicio21.py

---
+++ python/C/ejercicio21.py
@@ -0,0 +1,4 @@
+#The Auteco motorcycle company has a mid-year promotion, which consists of the following. Honda motorcycles have a discount of 4%, Yamaha of 7% and Suzuki of 15%. of 4%, Yamaha 7% and Suzuki 15%, the other brands 3%.
+
+mark= input("Enter the motocycle brand: ")
+price= int(input("Enter the price of the motocycle: "))
